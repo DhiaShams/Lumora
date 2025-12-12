bb45ecbd19fcb124e7e967c583d023c17213d497
DhiaShams <dhiadhilu123@gmail.com>
2025-12-12 20:58:27 +0530

    Initial folder setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from './Pages/Navbar/Navbar.jsx'
+import Hero from'./Pages/Hero/Hero';
+import About from './Pages/About/About'
+import Timeline from './Pages/Timeline/Timeline.jsx' 
+import Footer from './Pages/Footer/Footer.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />      
+      <About/>
+      <Timeline />
+      <Footer />
+    </>
+  )
+}
+
+export default App
