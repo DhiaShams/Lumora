63fadf006bf0a7dbbb5b2f13b91718d1c0a929e7
DhiaShams <dhiadhilu123@gmail.com>
2025-12-13 19:59:27 +0530

    Initial folders setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import Navbar from './Pages/Navbar/Navbar.jsx'
-import Hero from'./Pages/Hero/Hero';
-import About from './Pages/About/About'
-import Timeline from './Pages/Timeline/Timeline.jsx' 
+import Navbar from './Pages/Navbar/Navbar.jsx';
+import Hero from './Pages/Hero/Hero';
+import About from './Pages/About/About';
+import Event_info from './Pages/Event_info/Event_info.jsx';
+import Timeline from './Pages/Timeline/Timeline.jsx';
+import FAQ from './Pages/FAQ/faq.jsx';
 import Footer from './Pages/Footer/Footer.jsx';
 
 function App() {
-
   return (
     <>
       <Navbar />
-      <Hero />      
-      <About/>
+      <Hero />
+      <About />
+      <Event_info />
       <Timeline />
+      <FAQ />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
